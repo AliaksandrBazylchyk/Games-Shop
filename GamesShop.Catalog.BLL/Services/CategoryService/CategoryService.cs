2c1caff7a074cf4f67ef991e636076e08b7c8e3b
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2022-05-04 17:21:36 +0300

    Catalog business logic layer [unfinished]

---
+++ GamesShop.Catalog.BLL/Services/CategoryService/CategoryService.cs
@@ -0,0 +1,38 @@
+﻿using GamesShop.Catalog.DAL.Entities;
+using GamesShop.Catalog.DAL.Repositories.CategoryRepository;
+
+namespace GamesShop.Catalog.BLL.Services.CategoryService
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryService(
+            ICategoryRepository categoryRepository
+        )
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task<Category> GetByIdAsync(Guid id)
+        {
+            return await _categoryRepository.GetByIdAsync(id);
+        }
+
+        public async Task<IEnumerable<Category>> GetAllAsync()
+        {
+            return await _categoryRepository.GetAllAsync();
+        }
+
+        public async Task<Category> CreateAsync(string name)
+        {
+            var category = new Category
+            {
+                Id = Guid.NewGuid(), 
+                Name = name
+            };
+
+            return await _categoryRepository.CreateAsync(category);
+        }
+    }
+}
---
+++ GamesShop.Catalog.BLL/Services/CategoryService/ICategoryService.cs
@@ -0,0 +1,11 @@
+﻿using GamesShop.Catalog.DAL.Entities;
+
+namespace GamesShop.Catalog.BLL.Services.CategoryService
+{
+    public interface ICategoryService
+    {
+        Task<Category> GetByIdAsync(Guid id);
+        Task<IEnumerable<Category>> GetAllAsync();
+        Task<Category> CreateAsync(string name);
+    }
+}
