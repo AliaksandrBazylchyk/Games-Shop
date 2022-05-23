63f01830824aa217d4398d8ae2ef0d750219b6b8
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2022-05-23 15:23:31 +0300

    Created generic controller

---
+++ GamesShop.IdentityServer/Controllers/Account/AccountController.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using Duende.IdentityServer.Services;
+using Duende.IdentityServer.Stores;
+using GamesShop.IdentityServer.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GamesShop.IdentityServer.Controllers.Account
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<UserEntity> _userManager;
+        private readonly SignInManager<UserEntity> _signInManager;
+        private readonly IIdentityServerInteractionService _interaction;
+        private readonly IClientStore _clientStore;
+        private readonly IAuthenticationSchemeProvider _schemeProvider;
+        private readonly IEventService _events;
+        private readonly IMapper _mapper;
+
+        public AccountController(
+            UserManager<UserEntity> userManager,
+            SignInManager<UserEntity> signInManager,
+            IIdentityServerInteractionService interaction,
+            IClientStore clientStore,
+            IAuthenticationSchemeProvider schemeProvider,
+            IEventService events, 
+            IMapper mapper, 
+            RoleManager<RoleEntity> roleManager
+            )
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _interaction = interaction;
+            _clientStore = clientStore;
+            _schemeProvider = schemeProvider;
+            _events = events;
+            _mapper = mapper;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] RegisterUserModel model)
+        {
+            var user = _mapper.Map<RegisterUserModel, UserEntity>(model);
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                // await _userManager.AddToRoleAsync(user, "User");
+            }
+
+            return Ok(result);
+
+        }
+    }
+}
---
+++ GamesShop.IdentityServer/Mapping/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using GamesShop.IdentityServer.Controllers.Account;
+using GamesShop.IdentityServer.Models;
+
+namespace GamesShop.IdentityServer.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public override string ProfileName => "ModelsMappings";
+
+        public MappingProfile()
+        {
+            CreateMap<RegisterUserModel, UserEntity>();
+        }
+    }
+}
